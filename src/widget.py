826a576cde964f984a6daa27e96885d156251c78
Andrey Metelsky <metand1993@mail.ru>
2024-10-14 14:52:45 +0300

    added get_date func and docstrings

--- src/widget.py
+++ src/widget.py
@@ -2,7 +2,13 @@ from masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(card_info: str) -> str:
+    """Принимает строку с названием и номером карты или номер счета и возвращает маску карты или счета"""
     if "Счет" in card_info:
         return f"Счет {get_mask_account(int(card_info[4:]))}"
     else:
         return str(card_info[0:-16] + get_mask_card_number(card_info[-16:]))
+
+
+def get_date(date: str) -> str:
+    """Принимает строку с датой и возвращает ее в формате ДД.ММ.ГГГГ"""
+    return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
