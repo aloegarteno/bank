72da7b74bbc7798a26e5e9af91b5dac4104d37e7
Andrey Metelsky <metand1993@mail.ru>
2024-10-14 14:39:07 +0300

    widget.py fixes

--- src/widget.py
+++ src/widget.py
@@ -1,9 +1,8 @@
-from masks import get_mask_account
-from masks import get_mask_card_number
+from masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(card_info: str) -> str:
-    if 'Счет' in card_info:
-        return f'Счет {get_mask_account(int(card_info[4:]))}'
+    if "Счет" in card_info:
+        return f"Счет {get_mask_account(int(card_info[4:]))}"
     else:
-        return card_info[0:-16] + get_mask_card_number(card_info[-16:])
+        return str(card_info[0:-16] + get_mask_card_number(card_info[-16:]))
