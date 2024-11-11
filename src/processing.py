2f4b56f14e3d48ce8cc6d068b1e9ab173b1182b2
Andrey Metelsky <metand1993@mail.ru>
2024-11-11 13:39:28 +0300

    added sort_by_date func

--- src/processing.py
+++ src/processing.py
@@ -8,3 +8,10 @@ def filter_by_state(list_of_operations: Iterable[list], default_state: Any = "EX
         if operation_status.get("state") == default_state:
             executed_operations.append(operation_status)
     return executed_operations
+
+
+def sort_by_date(list_of_dicts: Iterable[list], reverse_list: bool = True) -> Iterable[list]:
+    """Принимает список словарей операций и возвращает отсортированный список операций(По умолчанию - убывание)"""
+    return sorted(list_of_dicts, key=lambda x: x["date"], reverse=reverse_list)
+
+
