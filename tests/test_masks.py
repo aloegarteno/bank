276d0cf534bff1f8153b51c32dab734089f4cae7
Andrey Metelsky <metand1993@mail.ru>
2024-11-28 15:45:31 +0300

    updated tests

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,6 +1,7 @@
 from src.masks import get_mask_account, get_mask_card_number
 
-def test_get_mask_card_number(card_number,wrong_card_number):
+
+def test_get_mask_card_number(card_number, wrong_card_number):
     assert get_mask_card_number(1111111111111111) == card_number
     assert get_mask_card_number(1111) == wrong_card_number
 
